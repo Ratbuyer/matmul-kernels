2366c4f381eb5797d10417e18e7c0a6ca20b7749
Yicong Eddy Luo <m1662576219@gmail.com>
2024-11-09 20:29:19 -0500

    script

---
+++ script.sh
@@ -0,0 +1,11 @@
+kernels=(1 2 3 4 5 6)
+N=(512 1024 4096)
+
+# ./run 512 512 512 100 6 0
+
+for kernel in ${kernels[@]}; do
+	for n in ${N[@]}; do
+		echo "Running kernel $kernel with N=$n"
+		./run $n $n $n 100 $kernel 0
+	done
+done
